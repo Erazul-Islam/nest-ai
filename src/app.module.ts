c8f049bd6aa51f2b68dd33b6220013a2779f97eb
Erazul Islam <erazul89@gmail.com>
2025-07-22 10:59:39 +0600

    create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { EmailModule } from './email/email.module';
 import { ImageModule } from './image/image.module';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), EmailModule, ImageModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    EmailModule,
+    ImageModule,
+    UserModule,
+    MongooseModule.forRoot(process.env.DATABASE_URL as string),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

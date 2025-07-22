52041c3dda413878667d62e5cbfdbbca0e29948e
Erazul Islam <erazul89@gmail.com>
2025-07-22 11:26:54 +0600

    hashed passowrd

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,13 +3,23 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './user.schema';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
+    const saltRounds = 10;
+    const password = createUserDto.password;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const userInfo = {
+      ...createUserDto,
+      password: hashedPassword,
+    };
+
+    const createdUser = new this.userModel(userInfo);
     return createdUser.save();
   }
 
